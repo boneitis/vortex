37f609f5337b4e88020d4b210d57c0d19cb8978f
js <some@handsome.guy>
2018-11-25 07:35:41 -0800

    00solve

---
+++ 00.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import socket
+import struct
+
+s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+
+try:
+    host = socket.gethostbyname('vortex.labs.overthewire.org')
+    port = 5842
+    s.connect( (host, port) )
+    in1 = s.recv(4)
+    in2 = s.recv(4)
+    in3 = s.recv(4)
+    in4 = s.recv(4)
+
+    int1 = struct.unpack('<L', in1)[0]
+    int2 = struct.unpack('<L', in2)[0]
+    int3 = struct.unpack('<L', in3)[0]
+    int4 = struct.unpack('<L', in4)[0]
+
+    s.send( struct.pack('<L', int1+int2+int3+int4) )
+
+    print( str(s.recv(1024)) )
+
+except:
+    print("kaboom")
+
